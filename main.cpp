514e6b5d4a0df328a3fbad4ded6643a5dcf31e40
Joshua <jne0010@uah.edu>
2021-03-26 23:10:39 -0500

    Made the parseXML function callable. Will put into a header file for use outside later

--- main.cpp
+++ main.cpp
@@ -9,29 +9,37 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+void
+parseXML(std::ifstream & stream1, std::ofstream & stream2)
+{
+    std::string data;
+    std::getline(stream1, data);
+
+    std::string delimiter = "<row";
+    size_t pos = 0;
+    std::string token;
+
+    while ((pos = data.find(delimiter)) != std::string::npos)
+    {
+        token = data.substr(0, pos) + "\n";
+        stream2 << token;
+        data.erase(0, pos + delimiter.length());
+    }
+    stream2 << token;
+
+    return;
+}
+
 int main()
 {
     std::ifstream stream1("data.xml");
     std::ofstream stream2("data.txt", std::ios::app);
-    std::string data;
 
-    std::getline(stream1, data);
+    parseXML(stream1, stream2);
 
-    std::string delimiter = "<";
+    cout << "Finsihed";
+
     std::string a;
-    size_t pos = 0;
-    std::string token;
-
-    while ((pos = data.find(delimiter[0])) != std::string::npos)
-    {
-        token = data.substr(0, pos) + "\n";
-        stream2 << token ;
-        std::cout << token << std::endl;
-        data.erase(0, pos + delimiter.length());
-    }
-
-    std::cout << data << std::endl;
-
     cin >> a;
 
     return 0;
