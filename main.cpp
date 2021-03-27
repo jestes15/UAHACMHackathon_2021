a0c78a879f77e5b33ed10c07e38bb027a825fd57
Joshua <jne0010@uah.edu>
2021-03-27 16:37:17 -0500

    Added the libxml files and the parseXML class

--- main.cpp
+++ main.cpp
@@ -4,38 +4,19 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "libxml.h"
 
 using std::cout;
 using std::endl;
 using std::cin;
 
-void
-parseXML(std::ifstream & stream1, std::ofstream & stream2)
-{
-    std::string data;
-    std::getline(stream1, data);
-
-    std::string delimiter = "<row";
-    size_t pos = 0;
-    std::string token;
-
-    while ((pos = data.find(delimiter)) != std::string::npos)
-    {
-        token = data.substr(0, pos) + "\n";
-        stream2 << token;
-        data.erase(0, pos + delimiter.length());
-    }
-    stream2 << token;
-
-    return;
-}
-
 int main()
 {
     std::ifstream stream1("data.xml");
     std::ofstream stream2("data.txt", std::ios::app);
+    parseXML xml_obj;
 
-    parseXML(stream1, stream2);
+    xml_obj.parseXML_s(stream1, stream2, "<row");
 
     cout << "Finsihed";
 
