f2393cbcd33309243888a8a863cfcabf0e961605
Qeolvdxu <hve0002@uah.edu>
2021-03-28 03:15:30 -0500

    added daysinyear function

--- header/graph.h
+++ header/graph.h
@@ -1,7 +1,7 @@
 #ifndef GRAPH_HEADER
 #define GRAPH_HEADER
 
-int graph(int[][2][10][50], int type, int state, int total);
+int graph(int[][2][10][50], int array_length, int type, int state, int total, int year);
 
 #include "../source/graph.cpp"
 
--- source/graph_TEST.cpp
+++ source/graph_TEST.cpp
@@ -3,8 +3,36 @@
 int main(void)
 {
 	
-	int testArray[100][2][10][50];
-    for (int i = 0; i < 50; i++)
+	int testArray[10][2][10][50];
+
+// Manually set Dates
+    testArray[0][1][4][5] = 20201005;
+    testArray[1][1][4][5] = 20201005;
+    testArray[2][1][4][5] = 20201030;
+    testArray[3][1][4][5] = 20201029;
+    testArray[4][1][4][5] = 20201023;
+    testArray[5][1][4][5] = 20201028;
+    testArray[6][1][4][5] = 20201010;
+    testArray[7][1][4][5] = 20201020;
+    testArray[8][1][4][5] = 20201010;
+    testArray[9][1][4][5] = 20201011;
+
+// Manually set the Data
+    testArray[0][0][4][5] = 101000;
+    testArray[1][0][4][5] = 105000;
+    testArray[2][0][4][5] = 208000;
+    testArray[3][0][4][5] = 309000;
+    testArray[4][0][4][5] = 303000;
+    testArray[5][0][4][5] = 505000;
+    testArray[6][0][4][5] = 602000;
+    testArray[7][0][4][5] = 403000;
+    testArray[8][0][4][5] = 507000;
+    testArray[9][0][4][5] = 209000;
+
+	graph(testArray,10, 4, 5, 100, 2021);
+
+
+/*    for (int i = 0; i < 50; i++)
     {
         for (int j = 0; j < 10;j++)
         {
@@ -16,8 +44,7 @@ int main(void)
                 }
             }
         }
-	}
+	}	*/
 
-	graph(testArray,0, 0, 100);
 	return 0;
 }
